f172c93d12c337f0cd3d69eeb834485cf9e5d7f8
Willian Felipe <w.felipebraz@gmail.com>
2022-06-08 19:54:03 -0300

    questão 1 feita :)

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,9 +1,17 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (param) => {
+  try {
+    if (param === undefined) { throw new Error('You must provide an url'); }
+    const link = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
+const response = await fetch(link);
+const data = await response.json();
+return data;
+  } catch (error) {
+    return error;
+  }
 };
-
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
   };
 }
+// console.log(fetchProducts('computador'));
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,21 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
- 'it()'
-  fail('Teste vazio');
-});
+ it('fetchProducts é uma função', () => {
+   expect(typeof fetchProducts).toBe('function')
+
+}); it(`Execute a função fetchProducts com o argumento 'computador' e teste se fetch foi chamada;` , async () => {
+  fetchProducts('computador');
+   expect(fetch).toHaveBeenCalled();
+})
+  it(`Teste se, ao chamar a função fetchProducts com o argumento 'computador', a função fetch utiliza o endpoint` , async () => {
+    fetchProducts('computador');
+    expect(fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador'))
+  })
+  it(`Teste se o retorno da função fetchProducts com o argumento 'computador' é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.`, async () => {
+    expect(await fetchProducts('computador')).toBe(computadorSearch);
+  })
+  it(`Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: 'You must provide an url'` , async () => {
+    expect(await fetchProducts()).toEqual(new Error('You must provide an url'))
+  })
+;})

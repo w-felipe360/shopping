1d725ac4e55575c812f92cc8be1a46b586c389d5
Willian Felipe <w.felipebraz@gmail.com>
2022-06-16 20:01:41 -0300

    questao 3 feita

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (param) => {
+  try {
+    if (param === undefined) { throw new Error('You must provide an url'); }
+    const link = `https://api.mercadolibre.com/items/${param}`;
+const response = await fetch(link);
+const data = await response.json();
+return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
